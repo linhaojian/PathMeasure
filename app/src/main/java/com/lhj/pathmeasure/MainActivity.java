e26ff8451dfbed9517cb1670e19cfffae04d459f
linhaojian <15989201703@163.com>
2018-03-20 14:54:16 +0800

    Initial commit

---
+++ app/src/main/java/com/lhj/pathmeasure/MainActivity.java
@@ -0,0 +1,17 @@
+package com.lhj.pathmeasure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
